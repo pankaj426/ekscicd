b5d935f7d4b1ee0f33801e127fdc42fbfffeb295
Pankaj Kumar <pankajjaiswal1456@gmail.com>
2021-07-27 19:52:00 +0530

    v1.06

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Eks pipeline is over now')
+  res.send('Eks pipeline is over now after error')
 })
 
 app.get('/status', (req, res) => {
